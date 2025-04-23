0528b2981544f806574105773de840dfb520ea00
Chance Tracker <cudiaf@gmail.com>
2025-04-23 03:40:53 +0300

    Delete help_handler.py

--- help_handler.py
+++
@@ -1,4 +0,0 @@
-def register(bot):
-    @bot.message_handler(commands=['help'])
-    def help(message):
-        bot.send_message(message.chat.id, "ℹ️ Questo bot ti aiuta a simulare un sistema sulla roulette europea.")
