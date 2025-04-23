a7e5b1d9bb0942f8eb779f31eec7fd41916c4a58
Chance Tracker <cudiaf@gmail.com>
2025-04-23 16:38:17 +0300

    Update keyboard.py

--- messages/keyboard.py
+++ messages/keyboard.py
@@ -4,4 +4,5 @@ def get_main_keyboard():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
     keyboard.row("☰ Menu", "🎲 Gioca")
     keyboard.row("↩️ Annulla", "📊 Statistiche")
+    keyboard.row("🔄 Reset")
     return keyboard
