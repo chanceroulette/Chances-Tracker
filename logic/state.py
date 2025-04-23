8c8b7c53f08d644dd39243714332dbeb09f3f226
Chance Tracker <cudiaf@gmail.com>
2025-04-23 23:22:43 +0300

    Update state.py

--- logic/state.py
+++ logic/state.py
@@ -3,7 +3,7 @@
 # Lista numeri inseriti per l'analisi
 user_data = {}
 
-# Fase attuale del gioco per ogni utente (es. 'analisi', 'gioco')
+# Fase attuale del gioco per ogni utente (es. 'analisi', 'gioco', 'selezione')
 game_phase = {}
 
 # Chances selezionate per ogni utente
@@ -11,3 +11,6 @@ selected_chances = {}
 
 # Struttura dei box di fiches per ogni chance attiva
 user_boxes = {}
+
+# Backup dei dati per annullare l’ultima giocata
+backup_data = {}
