1e0087b24525e8184f5d732bafccfcfa3ac74ed0
Chance Tracker <cudiaf@gmail.com>
2025-04-23 04:03:59 +0300

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,4 +1,11 @@
+from messages.welcome import get_welcome_message
+from messages.keyboard import get_main_keyboard
+
 def register(bot):
     @bot.message_handler(commands=['start'])
     def start(message):
-        bot.send_message(message.chat.id, "🎲 Benvenuto su ChanceTracker Bot!")
+        bot.send_message(
+            message.chat.id,
+            get_welcome_message(),
+            reply_markup=get_main_keyboard()
+        )
