3e1754c62ad51ada1decb63e49f1fabfb75aeae3
Chance Tracker <cudiaf@gmail.com>
2025-04-23 04:03:16 +0300

    Create keyboard.py

---
+++ messages/keyboard.py
@@ -0,0 +1,7 @@
+from telebot import types
+
+def get_main_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    keyboard.row("☰ Menu", "🎲 Gioca")
+    keyboard.row("↩️ Annulla", "📊 Statistiche")
+    return keyboard
