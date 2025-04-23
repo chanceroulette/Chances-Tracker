aafccc8846e819ad28db6b394de4bbe8f3e265a0
Chance Tracker <cudiaf@gmail.com>
2025-04-23 17:14:06 +0300

    Update state.py

--- logic/state.py
+++ logic/state.py
@@ -2,9 +2,13 @@
 
 user_data = {}
 backup_data = {}
+active_chances = {}
 
 def get_user_data():
     return user_data
 
 def get_backup_data():
     return backup_data
+
+def get_active_chances():
+    return active_chances
