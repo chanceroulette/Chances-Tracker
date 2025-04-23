b0ca57dbd590f212b07b02f08e5906fed0065c55
Chance Tracker <cudiaf@gmail.com>
2025-04-23 03:41:52 +0300

    Delete handlers/test_handler.py

--- handlers/test_handler.py
+++
@@ -1,7 +0,0 @@
-from telebot.types import Message
-
-def register_test(bot):
-    @bot.message_handler(func=lambda message: True)
-    def handle_test(message: Message):
-        print(f"📩 Messaggio ricevuto: {message.text} da {message.chat.id}")
-        bot.send_message(message.chat.id, "✅ Ricevuto!")
