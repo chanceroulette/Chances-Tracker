b0db2aafef479cb6337b28e5455784f6976dd26e
Chance Tracker <cudiaf@gmail.com>
2025-04-24 09:56:52 +0300

    Update keyboard_analisi.py

--- messages/analysis/keyboard_analisi.py
+++ messages/analysis/keyboard_analisi.py
@@ -1,11 +1,19 @@
-# messages/analysis/keyboard.py
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def get_numeric_keyboard():
+    """
+    Tastiera numerica da 0 a 36 per l’inserimento dei numeri della roulette.
+    """
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=6)
     buttons = [KeyboardButton(str(i)) for i in range(37)]
-    for i in range(0, 36, 6):
-        keyboard.row(*buttons[i:i+6])
-    keyboard.row(KeyboardButton("📊 Analizza ora"))
-    keyboard.row(KeyboardButton("☰ Menu"))
+    for i in range(0, 37, 6):
+        keyboard.add(*buttons[i:i+6])
+    return keyboard
+
+def get_analysis_keyboard():
+    """
+    Tastiera con il pulsante per avviare l'analisi dopo aver inserito almeno 10 numeri.
+    """
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(KeyboardButton("📊 Analizza"))
     return keyboard
